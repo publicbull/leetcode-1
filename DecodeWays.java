30588ea3e523587e7212458e1c07be0c59058404
Yandong Liu <askjupiter@gmail.com>
2013-05-04 00:30:37 -0700

    up

--- DecodeWays.java
+++ DecodeWays.java
@@ -2,85 +2,45 @@ public class Solution {
     public int numDecodings(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int n = s.length();
-        if(n==0) return 0;
-        int[] dp = new int[n];
-        boolean[] valid = new boolean[n];
-        if(s.charAt(0)=='0') {
-            dp[0]=0;
-            valid[0]=false;
-        }
-        else {
-            dp[0]=1;
-            valid[0]=true;
-        }
-        for(int i=1;i<n;i++) {
-            char c = s.charAt(i);
-            char c1 = s.charAt(i-1);
-            if(c=='0') {//00-90 (10,20 are valid)
-                if(c1=='1'||c1=='2') {
-                    if(i<2) {
-                        valid[i]=true;
-                        dp[i]=1;
-                    } else {
-                        if(valid[i-2]) {
-                            valid[i]=true;
-                            dp[i]=dp[i-2];
-                        } else {
-                            valid[i]=false;
-                            dp[i]=0;
-                        }
-                    }
-                } else {
-                    valid[i]=false;
-                    dp[i]=0;
-                }
-            } else if (c1=='1') {//11 - 19
-                if(i<2) {
-                    valid[i]=true;
-                    dp[i]=2;
-                } else if(valid[i-2]) {
-                    valid[i]=true;
-                    dp[i]=dp[i-1]+dp[i-2];
-                } else {
-                    valid[i]=false;
-                    dp[i]=0;
-                }
-            } else if(c1=='2') {
-                if(c>='1'&&c<='6') {
-                    if(i<2) {
-                        valid[i]=true;
-                        dp[i]=2;
-                    } else if(valid[i-2]) {
-                        valid[i]=true;
-                        dp[i]=dp[i-1]+dp[i-2];
-                    } else {
-                        valid[i]=false;
-                        dp[i]=0;
-                    }                    
-                } else {//27,28,29
-                    if(i<2) {
-                        valid[i]=true;
-                        dp[i]=1;
-                    } else if(valid[i-1]) {
-                        valid[i]=true;
-                        dp[i]=dp[i-1];
-                    }
-                    else {
-                        valid[i]=false;
-                        dp[i]=0;
-                    }          
-                }
-            } else {//3x,4x,...
-                valid[i]=valid[i-1];
-                dp[i]=dp[i-1];
-            }    
-        }
-        if(valid[n-1])
-            return dp[n-1];
-        else
-            return 0;
-        
-    }
+       if(s.length()==0) return 0;
+       int rlt =_numDecodings(s);
+
+       return rlt;
+   }
+   
+   
+   public int _numDecodings(String s){
+       int l=s.length();
+       int[] ways = new int[l+1];
+       
+       ways[0]=1;
+        int v = Integer.parseInt(s.substring(0,1));
+       if(v==0)
+           ways[1]= 0;
+       else
+           ways[1] =1;
+       
+
+       
+       for(int i=2;i<l+1;i++){        
+           String prefix;
+           prefix = s.substring(i-2,i);
+            v = Integer.parseInt(prefix);
+           if(v<=26&&v>0 && !prefix.startsWith("0")){
+               ways[i]=ways[i-2];
+           }else{
+               ways[i]=0;
+           }  
+           
+           prefix = s.substring(i-1,i);
+           if(!prefix.equals("0")){
+               ways[i] +=ways[i-1];
+           }            
+           
+           
+       }
+       
+       return ways[l];
+   }
 }
 
